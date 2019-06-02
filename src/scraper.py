e0e622f6cb4ff9beb380340315cbe84b770199b0
Abhishek Sisodia <50292256+Kabhi-Abhi@users.noreply.github.com>
2019-06-02 21:25:59 +0530

    Update scraper.py

--- src/scraper.py
+++ src/scraper.py
@@ -1,35 +1,73 @@
 #!/usr/bin/env python3
 # -*- encoding: utf-8
 
-import time
-from selenium import webdriver
+from telethon.sync import TelegramClient
+from telethon.tl.functions.messages import GetDialogsRequest
+from telethon.tl.types import InputPeerEmpty
+import csv
 
-phone_number = input("Enter your phone number: ")
+api_id = 123456
+api_hash = 'YOUR_API_HASH'
+phone = input("Enter your Phone number with country code (like: +919999999999): ")
+client = TelegramClient(phone, api_id, api_hash)
 
-chrome = webdriver.Chrome("./chromedriver.exe")
-chrome.get("https://web.telegram.org/#/login")
-time.sleep(5)
+client.connect()
+if not client.is_user_authorized():
+    client.send_code_request(phone)
+    client.sign_in(phone, input('Enter the code: '))
 
-chrome.find_element_by_name("phone_number").send_keys(phone_number)
 
-chrome.find_element_by_class_name("login_head_submit_wrap").click()
+chats = []
+last_date = None
+chunk_size = 200
+groups=[]
+ 
+result = client(GetDialogsRequest(
+             offset_date=last_date,
+             offset_id=0,
+             offset_peer=InputPeerEmpty(),
+             limit=chunk_size,
+             hash = 0
+         ))
+chats.extend(result.chats)
 
-code = input("Enter Security Code: ")
+for chat in chats:
+    try:
+        if chat.megagroup== True:
+            groups.append(chat)
+    except:
+        continue
 
-chrome.find_element_by_class_name("btn btn-md btn-md-primary").click()
+print('Choose a group to scrape members from:')
+i=0
+for g in groups:
+    print(str(i) + '- ' + g.title)
+    i+=1
 
-chrome.find_element_by_name("phone_code").send_keys(code)
+g_index = input("Enter a Number: ")
+target_group=groups[int(g_index)]
 
-groups = chrome.find_elements_by_class_name("im_dialog_wrap")
+print('Fetching Members...')
+all_participants = []
+all_participants = client.get_participants(target_group, aggressive=True)
 
-for i in range(len(groups)):
-    print(f"{i}: {groups[i]}")
-
-index = int(input("Enter the index of the group you wish to see: "))
-
-groups[index].click()
-
-chrome.find_elements_by_class_name("tg_head_btn").click()
-
-chrome.delete_all_cookies()
-chrome.quit()
+print('Saving In file...')
+with open("members.csv","w",encoding='UTF-8') as f:
+    writer = csv.writer(f,delimiter=",",lineterminator="\n")
+    writer.writerow(['username','user id', 'access hash','name','group', 'group id'])
+    for user in all_participants:
+        if user.username:
+            username= user.username
+        else:
+            username= ""
+        if user.first_name:
+            first_name= user.first_name
+        else:
+            first_name= ""
+        if user.last_name:
+            last_name= user.last_name
+        else:
+            last_name= ""
+        name= (first_name + ' ' + last_name).strip()
+        writer.writerow([username,user.id,user.access_hash,name,target_group.title, target_group.id])      
+print('Members scraped successfully.')
