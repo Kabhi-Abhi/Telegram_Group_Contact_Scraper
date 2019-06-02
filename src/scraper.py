69942a1ce24531ae7862802adc721a1fd1de3fa1
Abhishek Sisodia <50292256+Kabhi-Abhi@users.noreply.github.com>
2019-06-02 22:35:10 +0530

    Update scraper.py
    Experimental code from Telethon docs

--- src/scraper.py
+++ src/scraper.py
@@ -2,72 +2,37 @@
 # -*- encoding: utf-8
 
 from telethon.sync import TelegramClient
-from telethon.tl.functions.messages import GetDialogsRequest
-from telethon.tl.types import InputPeerEmpty
-import csv
+import json
+import logging
+logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s',
+                    level=logging.DEBUG)
 
-api_id = 123456
-api_hash = 'YOUR_API_HASH'
-phone = input("Enter your Phone number with country code (like: +919999999999): ")
-client = TelegramClient(phone, api_id, api_hash)
+# Use your own values from my.telegram.org
+api_id = 12345
+api_hash = '0123456789abcdef0123456789abcdef'
+phone = input("Enter your phone number with country code (like +919999999999): ")
 
-client.connect()
-if not client.is_user_authorized():
-    client.send_code_request(phone)
-    client.sign_in(phone, input('Enter the code: '))
-
-
-chats = []
-last_date = None
-chunk_size = 200
-groups=[]
- 
-result = client(GetDialogsRequest(
-             offset_date=last_date,
-             offset_id=0,
-             offset_peer=InputPeerEmpty(),
-             limit=chunk_size,
-             hash = 0
-         ))
-chats.extend(result.chats)
-
-for chat in chats:
-    try:
-        if chat.megagroup== True:
-            groups.append(chat)
-    except:
-        continue
-
-print('Choose a group to scrape members from:')
-i=0
-for g in groups:
-    print(str(i) + '- ' + g.title)
-    i+=1
-
-g_index = input("Enter a Number: ")
-target_group=groups[int(g_index)]
-
-print('Fetching Members...')
-all_participants = []
-all_participants = client.get_participants(target_group, aggressive=True)
-
-print('Saving In file...')
-with open("members.csv","w",encoding='UTF-8') as f:
-    writer = csv.writer(f,delimiter=",",lineterminator="\n")
-    writer.writerow(['username','user id', 'access hash','name','group', 'group id'])
-    for user in all_participants:
-        if user.username:
-            username= user.username
-        else:
-            username= ""
-        if user.first_name:
-            first_name= user.first_name
-        else:
-            first_name= ""
-        if user.last_name:
-            last_name= user.last_name
-        else:
-            last_name= ""
-        name= (first_name + ' ' + last_name).strip()
-        writer.writerow([username,user.id,user.access_hash,name,target_group.title, target_group.id])      
-print('Members scraped successfully.')
+# The first parameter is the .session file name (absolute paths allowed)
+with TelegramClient(phone, api_id, api_hash) as client:
+    client.send_message('me', 'Hello, successfully logged in using python client script!')
+    
+    dialogs = client.get_dialogs()
+    
+    groups = []
+    channel = []
+    for dialog in dialogs:
+        if dialog.is_group() and not dialog.is_channel():
+            groups.append(dialog)
+        elif dialog.is_channel():
+            channel.append()
+    users = {}
+    for group in groups:
+        users[group.title] = client.get_participants(group)
+    
+    for channel in channels:
+        users[channel.title] = client.get_participants(channel)
+    
+    with open("users.json", "w") as fp:
+        json.dump(users, fp, separators=(',', ':'), indent=4)
+    
+    client.run_until_disconnected()
